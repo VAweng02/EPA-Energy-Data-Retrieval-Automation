510656a8ea7ed57a06f8bfe2b5d8023843676e47
Vincent Weng <vincentweng@Vincents-MacBook-Pro.local>
2023-05-26 09:33:25 -0400

    started api

--- api.py
+++ api.py
@@ -1,70 +1,30 @@
 import requests
 import pandas as pd
 
-# These are examples of querying annual emissions data.
-
-####### Streaming services API #######
-
-# If you have a lot of data to request, use the streaming services API to avoid
-# pagination and much quicker collection of the data you need.
-
 API_KEY = 'YLClluBS4DJotWC0iBc7HV2b6SxdEcFtSbCu2s5x'
 
-# api parameters for the streaming facility/attributes endpoint
 parameters = {
     'api_key': API_KEY,
-    'year': 2021,
-    'programCodeInfo': 'CSOSG2|CSOSG3',
+    'beginDate': '2022-01-01',
+    'endDate': '2022-01-02',
+    'operatingHoursOnly': True,
+    'perPage': 500
 }
 
-# making get request using the emissions/apportioned/annual endpoint
-streamingUrl = "https://api.epa.gov/easey/streaming-services/emissions/apportioned/annual"
+streamingUrl = "https://api.epa.gov/easey/emissions-mgmt/emissions/apportioned/hourly/by-facility"
 streamingResponse = requests.get(streamingUrl, params=parameters)
 
-# printing useful info from the response
-print("Status code: "+str(streamingResponse.status_code))
-print("Field Mappings: "+str(streamingResponse.headers['X-Field-Mappings']))
-# collecting data as a data frame
-streamingResponse_df = pd.DataFrame(streamingResponse.json())
-print(streamingResponse_df)
+print("Status code:", streamingResponse.status_code)
 
-####### Emissions mgmt API #######
-# This is example shows how to page through results.
-
-import math
-
-# api parameters for the annual emissions
-parameters = {
-    'api_key': API_KEY,
-    'year': 2021,
-    'programCodeInfo': 'CSOSG2|CSOSG3',
-    'page': 1,
-    'perPage': 100
-}
-
-# making get request
-emissionsUrl = "https://api.epa.gov/easey/emissions-mgmt/emissions/apportioned/annual"
-emissionsResponse = requests.get(emissionsUrl, params=parameters)
-
-# Note: the x-total-count header is the total number of records in the response.
-# You can use this to determine if you need to make another request to get all the records.
-totalCount = emissionsResponse.headers['X-Total-Count']
-print("Status code: "+str(emissionsResponse.status_code))
-#print("Field Mappings: "+str(response.headers['X-Field-Mappings']))
-print("Total Count: "+str(totalCount))
-
-# print the first page of the data
-emissionsResponse_df = pd.DataFrame(emissionsResponse.json())
-print(emissionsResponse_df)
-
-# print other pages of the data if it's available
-if math.ceil(int(totalCount)/int(parameters['perPage'])) > 1:
-    for x in range(math.ceil(int(totalCount)/int(parameters['perPage']))-1):
-        parameters['page'] += 1
-        emissionsResponse = requests.get(emissionsUrl, params=parameters)
-        emissionsResponse_df = pd.DataFrame(emissionsResponse.json())
-        # print(emissionsResponse_df)
-
-# Export dataframes to CSV
-streamingResponse_df.to_csv('streaming_data.csv', index=False)
-emissionsResponse_df.to_csv('emissions_data.csv', index=False)
+if streamingResponse.status_code == 200:
+    try:
+        streamingResponse_data = streamingResponse.json()
+        # Inspect the data structure
+        print(streamingResponse_data)
+        # Convert the data to a DataFrame
+        streamingResponse_df = pd.DataFrame(streamingResponse_data)
+        print(streamingResponse_df)
+    except Exception as e:
+        print("Error occurred while processing the response:", str(e))
+else:
+    print("Request failed with status code:", streamingResponse.status_code)
